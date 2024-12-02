fdd3ad15c807c34466e457ba903608a1cd495b49
Kevin La <46628649+Kevlar0150@users.noreply.github.com>
2024-12-02 22:36:56 +0000

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+import client from "../../lib/apollo";
+import { GET_POSTS } from "../../lib/queries";
+
+export default async function Home() {
+  const { data } = await client.query({
+    query: GET_POSTS,
+  });
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold">Blog Posts</h1>
+      {data.posts.nodes.map((post) => (
+        <div key={post.slug} className="my-4 p-4 border rounded">
+          <h2 className="text-xl">{post.title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+//  test
