d1b49fdeeaf98c60e235ba7e7f56032d02e5de63
Kevin La <46628649+Kevlar0150@users.noreply.github.com>
2024-12-02 22:43:45 +0000

    Test auto deployment on netlify

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default async function Home() {
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl font-bold">Blog Posts</h1>
+      <h1 className="text-2xl font-bold">TEST NEXT JS</h1>
       {data.posts.nodes.map((post) => (
         <div key={post.slug} className="my-4 p-4 border rounded">
           <h2 className="text-xl">{post.title}</h2>
